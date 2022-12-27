b45ab4662cdbdd13e7174f70fa9ff10abfbc0525
Evokus <dan.faria00@gmail.com>
2022-12-27 23:43:55 +0000

    Created Switch Details UI

--- app/src/main/java/com/example/theswitcher_carlosfaria/SwitchDetailsActivity.kt
+++ app/src/main/java/com/example/theswitcher_carlosfaria/SwitchDetailsActivity.kt
@@ -2,10 +2,27 @@ package com.example.theswitcher_carlosfaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class SwitchDetailsActivity : AppCompatActivity() {
+
+    private lateinit var divisionName : String
+    private var divisionSwitchStatus = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_switch_details)
+
+        divisionName = intent.getStringExtra("divisionName").toString()
+        divisionSwitchStatus = intent.getBooleanExtra("divisionSwitchStatus", false)
+
+        val imageViewDivisionSwitch = findViewById<ImageView>(R.id.imageViewLight)
+        val textViewStatusTitle = findViewById<TextView>(R.id.textViewStatusTitle)
+        val textViewStatus = findViewById<TextView>(R.id.textViewStatus)
+
+        if(divisionSwitchStatus){
+            imageViewDivisionSwitch.setImageResource(R.drawable.img)
+        }
     }
 }
