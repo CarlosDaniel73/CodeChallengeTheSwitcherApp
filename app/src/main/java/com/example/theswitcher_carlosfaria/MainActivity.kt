1c3f691f694062c3821990b9a89bb7bb4919a808
Evokus <dan.faria00@gmail.com>
2022-12-27 15:10:00 +0000

    Created recycler view item UI with custom switcher looks

--- app/src/main/java/com/example/theswitcher_carlosfaria/MainActivity.kt
+++ app/src/main/java/com/example/theswitcher_carlosfaria/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.theswitcher_carlosfaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Sets the custom Action Bar
+        setSupportActionBar(findViewById(R.id.switcherToolbar))
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
     }
 }
